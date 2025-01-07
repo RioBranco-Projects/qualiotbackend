93240f36c2d9fcf1273fb61aa8c98d4eec010ccf
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2025-01-07 09:35:51 -0300

    add validação de limite de categorias de um so produto

--- src/service/category-service.js
+++ src/service/category-service.js
@@ -174,6 +174,24 @@ const CategoryService = {
             },
           };
         }
+
+        // Validar se as categorias do produto ja excederam o limite maximo
+
+        // Validar se tem o limite de categorias ja foi excedido
+      const limitCategory = 10;
+
+      const countCategory = await Category.countDocuments({
+        _idProduct: product._id,
+      });
+
+      if (countCategory >= limitCategory) {
+        return {
+          code: 400,
+          error: {
+            message: "category limit exceeded",
+          },
+        };
+      }
       }
 
       if (dataUpdate.name) {
